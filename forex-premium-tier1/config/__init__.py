21725c839a0f8f79520e488a3241b10f81bb0bc6
fahadsultanemirates-creator <fahad.sultanemirates@gmail.com>
2026-08-25 23:56:03 +0400

    Add forex-premium-tier1/config/__init__.py

---
+++ forex-premium-tier1/config/__init__.py
@@ -0,0 +1,4 @@
+"""Premium Tier 1 config package."""
+from .settings import PremiumSettings, MT5Config, RiskConfig, PREMIUM_MAGIC_NUMBER
+
+__all__ = ["PremiumSettings", "MT5Config", "RiskConfig", "PREMIUM_MAGIC_NUMBER"]
